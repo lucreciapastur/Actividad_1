9a9c8acfca3f469b4ddd82e7211264fb9e4f409b
lucreciapastur <lucreciapastur@gmail.com>
2025-03-21 21:12:50 -0300

    Creación de funcionalidad 2: cálculo de puntaje

--- questions.py
+++ questions.py
@@ -20,6 +20,7 @@ answers = [
     ("=", "==", "!=", "==="),
 ]
 correct_answers_index = [1, 2, 0, 3, 1]
+score=0
 for _ in range(3):
     question_index = random.randint(0, len(questions) - 1)
     print(questions[question_index])
@@ -31,14 +32,18 @@ for _ in range(3):
             user_answer = int(user_answer) - 1
             if user_answer < 0 or user_answer >= len(answers[question_index]):
                 print("Respuesta no válida")
+                score -=0.5
                 sys.exit(1)
             if user_answer == correct_answers_index[question_index]:
                 print("¡Correcto!")
+                score +=1
                 break
         else:
             print("Respuesta no válida")
+            score -=0.5
             sys.exit(1)
     else:
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
     print()
+print(f"Puntaje final: {score}")
