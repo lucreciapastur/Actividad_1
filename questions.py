198f05c1163170d37d9d9bf78463e6403da96723
lucreciapastur <lucreciapastur@gmail.com>
2025-03-21 21:03:35 -0300

    Creación del archivo inicial

---
+++ questions.py
@@ -0,0 +1,35 @@
+import random
+questions = [
+    "¿Qué función se usa para obtener la longitud de una cadena en Python?",
+    "¿Cuál de las siguientes opciones es un número entero en Python?",
+    "¿Cómo se solicita entrada del usuario en Python?",
+    "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
+    "¿Cuál es el operador de comparación para verificar si dos valores son iguales?",
+]
+answers = [
+    ("size()", "len()", "length()", "count()"),
+    ("3.14", "'42'", "10", "True"),
+    ("input()", "scan()", "read()", "ask()"),
+    (
+        "// Esto es un comentario",
+        "/* Esto es un comentario */",
+        "-- Esto es un comentario",
+        "# Esto es un comentario",
+    ),
+    ("=", "==", "!=", "==="),
+]
+correct_answers_index = [1, 2, 0, 3, 1]
+for _ in range(3):
+    question_index = random.randint(0, len(questions) - 1)
+    print(questions[question_index])
+    for i, answer in enumerate(answers[question_index]):
+        print(f"{i + 1}. {answer}")
+    for intento in range(2):
+        user_answer = int(input("Respuesta: ")) - 1
+        if user_answer == correct_answers_index[question_index]:
+            print("¡Correcto!")
+            break
+    else:
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[question_index][correct_answers_index[question_index]])
+    print()
