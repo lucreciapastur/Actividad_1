f7162bef5f9cf8543e3e3a2c214550ac473d3df1
lucreciapastur <lucreciapastur@gmail.com>
2025-03-21 21:17:50 -0300

    Creación de funcionalidad 4: preguntas sin repetir

--- questions.py
+++ questions.py
@@ -21,7 +21,7 @@ answers = [
 ]
 correct_answers_index = [1, 2, 0, 3, 1]
 score=0
-questions_to_ask=random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+questions_to_ask=random.sample(list(zip(questions, answers, correct_answers_index)), k=3)
 for question, answer_options, correct_index in questions_to_ask:
     print(question)
     for i, answer in enumerate(answer_options):
